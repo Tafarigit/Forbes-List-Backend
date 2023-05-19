a76d19aa15d112fe18e0ddfeb33c4f88782a1605
Tafari Excell <excell.tafari@gmail.com>
2023-05-19 12:15:48 -0400

    added new script in package.json

--- Controllers/giftsControllers.js
+++ Controllers/giftsControllers.js
@@ -23,8 +23,39 @@ gifts.get("/:id", async (req, res) => {
     } else if (supply.error.code === 0){
         res.status(404).send("Not Found")
 
+    } else {
+        res.status(500).json({error: "server error"});
     }
 })
+
+gifts.post("/", async (req, res) => {
+  try {
+  const newGift = await createGifts(req.body);
+  res.status(200).json(newGift);
+  console.log("new gift:", newGift);
+}
+catch (error) {
+  res.status(400).json({ error: error });
+}
+});  
+
+gifts.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedGift = await deleteGifts(id);
+  if (!deletedGift.id) {
+    res.status(404).json(deletedGift);
+  }else{
+  res.status(200).json(deletedGift);
+  }
+});
+
+gifts.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const {name, brand, price, quantity, description, is_favorite, is_wearable} = req.body; 
+  const updatedGift = await updateGifts(id, req.body);
+
+  res.status(200).json(updatedGift);
+});
 
 
 module.exports = gifts;
